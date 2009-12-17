b4b360f44570d3757036c3a5a3b4301be0b16cef
ewheeler <evanmwheeler@gmail.com>
2009-12-17 14:34:04 +0000

    handle notification messages from operators, working airtime transfers and balance checking

--- app.py
+++ app.py
@@ -9,6 +9,10 @@ except ImportError:
     # Python 2.6 includes json library
     import json
 
+from datetime import datetime
+
+from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
+
 import rapidsms
 from ussd.models import *
 
@@ -61,56 +65,138 @@ class App (rapidsms.app.App):
                     return network
         return None
 
+    def _get_network_by_identity(self, identity):
+        """ Find a network operator's JSON object by identity. """
+        for network in self.mobile_networks:
+            identities = network.get("Operator Identities")
+            if identities is not None:
+                if identity in identities:
+                    return network
+        return None
+
     def update_balance(self):
         self.debug('updating balances...')
         sims = SIM.objects.all()
-        balances = []
         for sim in sims:
             self.debug(sim.operator_name)
-            b = self.check_balance(sim.backend.slug, sim.operator_name)
-            sim.balance = unicode(b)
+            b = self.check_balance(sim)
+            sim.balance = b
             sim.save()
-            balances.append(unicode(b))
-        return balances
 
-    def check_balance(self, backend_slug, operator_short):
+    def check_balance(self, sim):
         self.debug('checking balance...')
-        network = self._get_network_by("Operator Short", operator_short)
+        network = self._get_network_by("Operator Short", sim.operator_name)
         if network is not None:
-            # TODO check for None
-            result = self._run_ussd(backend_slug, network["USSD Balance"])
+            result = self._run_ussd(sim.backend.slug, network["USSD Balance"])
             if result is not None:
                 self.debug(result)
+                notice = OperatorNotification(sim=sim, type='B', text=result,\
+                    identity='USSD')
+                notice.save()
+                result_list = result.split()
+                # return the first token that is a number and hope
+                # that its the airtime balance
+                for token in result_list:
+                    if token.isdigit():
+                        return token
+                # if there is no number in the result, return the
+                # whole string so it can be reviewed via the web
                 return result
+            return "Unknown. Please try again later."
 
-    def transfer_airtime(self, backend_slug, operator_short, destination, amount, pin):
-        network = self._get_network_by("Operator Short", operator_short)
-        if network is not None:
-            # TODO destination number must not include international prefix --
-            # be more clever than this..
-            if destination.startswith('+'):
-                return "Please try again without international prefix"
-            ussd_string = network["USSD Transfer"] % {'destination' : destination,\
-                'amount' : amount, 'PIN' : pin }
-            result = self._run_ussd(backend_slug, ussd_string)
-            if result is not None:
-                self.debug(result)
-                return result
+    def recharge_airtime(self, sim):
+        self.debug('recharging airtime...')
+        # TODO
+        pass
+
+    def transfer_airtime(self, sim, destination, amount, pin=""):
+        self.debug('transferring airtime...')
+        network = self._get_network_by("Operator Short", sim.operator_name)
+        # messages confirming transfers can be very vauge -- often not
+        # containing the intended destination -- so we will only initiate
+        # a new transfer if there are no outstanding transfers expecting
+        # a notification message
+        if self.pending_transfer(network) is None:
+            if network is not None:
+                # TODO destination number must not include international prefix --
+                # be more clever than this..
+                if destination.startswith('+'):
+                    return "Please try again without international prefix"
+
+                # assemble ussd_string
+                ussd_string = network["USSD Transfer"] % {'destination' : destination,\
+                    'amount' : amount, 'PIN' : pin }
+                # execute
+                result = self._run_ussd(sim.backend.slug, ussd_string)
+                self.debug('ussd executed!')
+                if result is not None:
+                    self.debug(result)
+                    trans = AirtimeTransfer(destination=destination,\
+                        amount=amount, sim=sim, initiated=datetime.now(),\
+                        result='P')
+                    trans.save()
+                    self.debug(trans)
+                    return result
+        else:
+            return "Please try again later."
+
 
     def handle(self, message):
-        if message.connection.identity.lower().startswith("orange"):
-            self.process_confirmation(message)
         if message.text.lower().startswith("balance"):
             self.debug(self.update_balance())
         if message.text.lower().startswith("send"):
             self.debug(self.send_ro_credit())
 
+        # if message's sender is an identity used by an operator,
+        # try to process as a notification 
+        network = self._get_network_by_identity(message.peer)
+        if network is not None:
+            return self.process_notification(message, network)
+
     def send_ro_credit(self):
         sim = SIM.objects.all()[0]
-        return self.transfer_airtime(sim.backend.slug, sim.operator_name,\
-            "772720297", "100", "")
+        return self.transfer_airtime(sim, "772720297", "100")
 
-    def process_confirmation(self, message):
-        self.debug('processing confirmation...')
+    def process_notification(self, message, network):
+        self.debug('processing notification...')
         self.debug(message.connection.identity)
         self.debug(message.text)
+
+        # if their notification prefix numberings are any indication,
+        # there may be thousands of kinds of notification messages...
+        notice_type = 'U'
+
+        # TODO these are Orange SN specific
+        # need to experiment with more operators to know how to
+        # do this sensibly
+        if message.text.startswith('202'):
+            # somebody sent us airtime
+            notice_type = 'R'
+        if message.text.startswith('2049'):
+            # our transfer attempt failed (not enough credit?)
+            notice_type = 'F'
+        if message.text.startswith('201'):
+            # our transfer succeeded!
+            notice_type = 'S'
+
+        sim = SIM.objects.get(operator_name=network["Operator Short"])
+
+        notification = OperatorNotification(text=message.text,\
+            identity=message.peer, type=notice_type, sim=sim)
+        notification.save()
+
+        pending = self.pending_transfer(network)
+        if isinstance(pending, AirtimeTransfer):
+            pending.notification = notification
+
+
+    def pending_transfer(self, network):
+        try:
+            pending_transfer = AirtimeTransfer.objects.get(\
+                sim__operator_name=network["Operator Short"], result='P')
+            return pending_transfer
+        except MultipleObjectsReturned:
+            return "MADNESS"
+        except ObjectDoesNotExist:
+            return None
+
--- models.py
+++ models.py
@@ -12,21 +12,51 @@ class SIM(models.Model):
     backend = models.ForeignKey(PersistantBackend, blank=True, null=True)
     balance = models.CharField(max_length=500, blank=True, null=True)
 
-class AirtimeTransaction(models.Model):
+    def __unicode__(self):
+        return "%s (%s)" % (self.operator_name, self.backend.slug)
+
+class OperatorNotification(models.Model):
+    NOTIFICATION_CHOICES = (
+        ('U', 'Unknown'),
+        ('R', 'Receive Airtime'),
+        ('S', 'Transfer Success'),
+        ('F', 'Transfer Failure'),
+        ('B', 'Airtime Balance'),
+    )
+    text = models.CharField(max_length=160, blank=True, null=True)
+    identity = models.CharField(max_length=160, blank=True, null=True)
     sim = models.ForeignKey(SIM)
-    amount = models.CharField(max_length=160)
-    confirmed = models.DateTimeField(blank=True,null=True)
+    received = models.DateTimeField(default=datetime.now, blank=True, null=True)
+    type = models.CharField(max_length=1, choices=NOTIFICATION_CHOICES, default='U')
+
+    def __unicode__(self):
+        return "%s: %s (%s)" % (self.sim.operator_name, self.get_type_display(), self.received)
+
+class AirtimeTransaction(models.Model):
+    TRANS_RESULT_CHOICES = (
+        ('P', 'Pending'),
+        ('S', 'Success'),
+        ('F', 'Failure'),
+        ('U', 'Unknown'),
+    )
+    sim = models.ForeignKey(SIM)
+    amount = models.CharField(max_length=160, blank=True, null=True)
+    result = models.CharField(max_length=1, choices=TRANS_RESULT_CHOICES, default='P')
+    initiated = models.DateTimeField(blank=True, null=True)
+    notification = models.ForeignKey(OperatorNotification, blank=True, null=True)
+
+    def __unicode__(self):
+        return "%s: %s CFA, %s" % (self.sim.operator_name, self.amount, self.get_result_display())
 
 class AirtimeTransfer(AirtimeTransaction):
-    destination = models.CharField(max_length=160)
-    initiated = models.DateTimeField(blank=True,null=True)
+    destination = models.CharField(max_length=160, blank=True, null=True)
 
     @property
     def crux(self):
         return self.destination
 
 class AirtimeRecharge(AirtimeTransaction):
-    recharge_code = models.CharField(max_length=160)
+    recharge_code = models.CharField(max_length=160, blank=True, null=True)
 
     @property
     def crux(self):
